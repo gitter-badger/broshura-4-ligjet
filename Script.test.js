b6ff7099162059763d78e4a484498cf222c89391
Gordon Burgett <gordon@gordonburgett.net>
2017-05-16 02:39:25 +0200

    add basic test setup using karma

---
+++ Script.test.js
@@ -0,0 +1,47 @@
+
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+describe('kontakt', () => {
+
+  beforeEach(function (done) {
+    console.log('loading')
+    // load the index.html page into the window
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", function() {
+      document.body.innerHTML = this.responseText
+      done()
+    });
+    oReq.open("GET", "base/index.html");
+    oReq.send();
+  });
+
+  afterEach(function () {
+    document.body.innerHTML = '<h1>between tests</h1>'
+  })
+
+  it('should set the form to visible', () => {
+
+    // act
+    kontakt();
+
+    // assert
+    expect(document.getElementById("forma")).to.exist
+    
+    var style = document.getElementById("forma").style
+    expect(style.visibility).to.equal('visible')
+  })
+
+  it('should reset the form to invisible', () => {
+
+    // act
+    kontakt();
+
+    // assert
+    expect(document.getElementById("forma")).to.exist
+    
+    var style = document.getElementById("forma").style
+    expect(style.visibility).to.equal('hidden')
+  })
+})
